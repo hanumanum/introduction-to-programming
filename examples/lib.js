6ed252eb6ca15dea30b56ac2f5316c5ff46d638e
hanuman <a.kolyan@gratun.am>
2017-10-19 10:48:41 +0000

    start of quee method, currently doe's not wirking

--- examples/lib.js
+++ examples/lib.js
@@ -1,47 +1,54 @@
 var canvas = document.getElementById('canv');
-var delay = 15;
-var delay1 = 15;
-var delay2 = 15;
-//context.globalCompositeOperation = "source-over";
+var queue = new Queue(); 
+var ind = 1;
+
+
+/*
+var interval = setInterval(function(){
+    ind++;
+    var f = queue.dequeue();
+    if(f!==undefined){
+        f.fill();
+        console.log(queue.getLength());
+    }
+
+    if(ind==100){
+        clearInterval(interval);
+    }
+
+},10)
+*/
+
 
 function drawSquare(x,y,w,color) {
-    delay+=5;
-    setTimeout(function(){
-        var context = canvas.getContext('2d');
-        context.beginPath();
-        //context.clearRect(x-1, y-1, 101, 101);
-        //context.clearRect(x+1, y-1, 101, 101);
-        //context.clearRect(x+1, y+1, 101, 101);
-        //context.clearRect(x-1, y+1, 101, 101);
-        
-        context.fillStyle = color;
-        context.rect(x, y, w, w);
-        context.fill();
-        context.lineWidth = 0;
-        //console.log(x,y);
-    }, delay);
-
+    var dr = _drawSquare(x,y,w,color);
+    queue.enqueue(dr);
+    //queue.list();
 }
 
 
+function _drawSquare(x,y,w,color){
+    var context = canvas.getContext('2d');
+    context.beginPath();
+    context.fillStyle = color;
+    context.rect(x, y, w, w);
+    //context.fill();
+    return context;
+}
+
 function drawCirlce(x,y,r,color){
-    delay1+=5;
-    setTimeout(function(){
+    var dr = _drawCirlce(x,y,r,color);
+    queue.enqueue(dr);
+    //queue.list();
+}
+
+function _drawCirlce(x,y,r,color){
         var context = canvas.getContext('2d');
         context.beginPath();
-        //context.clearRect(x-2*r, y-2*r, 101, 101);
-        //context.clearRect(x+2*r, y-2*r, 101, 101);
-        //context.clearRect(x-2*r, y+2*r, 101, 101);
-        //context.clearRect(x+2*r, y+2*r, 101, 101);
-        //context.clearRect(x+1, y-1, 101, 101);
-        //context.clearRect(x+1, y+1, 101, 101);
-        //context.clearRect(x-1, y+1, 101, 101);
         context.arc(x, y, r, 0, 2 * Math.PI, false);
         context.fillStyle = color;
-        context.fill();
-        //context.lineWidth = 0;
-        //console.log(x,y);
-    }, delay1);
+        //context.fill();
+        return context;
 }
 
 function random(min, max) {
@@ -64,16 +71,21 @@ function background(color){
     canv.style.backgroundColor = color;
 }
 
+
 function clear(){
-    delay2+=5
-    setTimeout(function(){
+    var dr = _clear();
+    queue.enqueue(dr);
+}
+
+
+function _clear(){
+    
         var context = canvas.getContext('2d');
         context.clearRect(0, 0, 1000, 1000);
-        }, delay2);
 
 }
 
-  //drawSquare(200,150);
+//drawSquare(200,150);
 
 
   /*
@@ -98,3 +110,43 @@ function clear(){
 
 
   */
+
+
+
+function Queue(){
+      var queue  = [];
+      var offset = 0;
+      this.getLength = function(){
+        return (queue.length - offset);
+      }
+      this.isEmpty = function(){
+        return (queue.length == 0);
+      }
+      this.enqueue = function(item){
+        queue.push(item);
+      }
+    
+      this.dequeue = function(){
+        if (queue.length == 0) return undefined;
+        var item = queue[offset];
+        if (++ offset * 2 >= queue.length){
+          queue  = queue.slice(offset);
+          offset = 0;
+        }
+    
+        return item;
+    
+      }
+    
+      /* Returns the item at the front of the queue (without dequeuing it). If the
+       * queue is empty then undefined is returned.
+       */
+      this.peek = function(){
+        return (queue.length > 0 ? queue[offset] : undefined);
+      }
+
+      this.list = function(){
+          console.log(queue);
+      }
+    
+    }
